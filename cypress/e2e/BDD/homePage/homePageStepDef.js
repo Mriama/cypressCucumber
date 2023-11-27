e6bb4e6ff33519a579c98f071e4600b3ce9caffd
Mariama Diallo <mdiallo@agiltoo.fr>
2023-11-27 10:25:22 +0100

    integration github action

--- cypress/e2e/BDD/homePage/homePageStepDef.js
+++ cypress/e2e/BDD/homePage/homePageStepDef.js
@@ -10,10 +10,12 @@ Then('I should have access to the courses', function() {
   // cy.get(this.data.coursUn).should('exist')
   // cy.get(this.data.coursDeux).should('exist')
   // cy.get(this.data.coursTrois).should('exist')
+  // cy.get(this.data.coursTrois).should('exist')
 
   // Boucle pour vérifier la présence des éléments
   for (let i = 0; i < 4; i++) {
     cy.get(this.data[`cours_${i}`]).should('exist');
 }
 })
+
 
